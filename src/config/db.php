2536603f067a116734f79596e8547ea4eda9f316
Juan Manuel Alvarado <76979404+Juan-Alvarado21@users.noreply.github.com>
2024-06-28 20:08:01 -0600

    Update db.php

--- src/config/db.php
+++ src/config/db.php
@@ -1,8 +1,3 @@
-<?php
-$servername = "localhost";
-$username = "juan";
-$password = "sp_penetreitor"; // Asegúrate de usar tu contraseña real
-$dbname = "SystemPlumberBD"; // Asegúrate de que este nombre sea correcto
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
