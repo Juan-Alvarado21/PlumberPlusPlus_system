0f675f763a56e30d32b3a1d098c527678f99b024
Ergusha11 <gustah901@gmail.com>
2024-06-28 22:34:23 -0600

    remove consuld

--- src/components/admin/scripts/manageUser.js
+++ src/components/admin/scripts/manageUser.js
@@ -1,32 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener referencia al botón de agregar usuario
+    const addUserBtn = document.querySelector('.add-user-btn');
+    addUserBtn.addEventListener('click', function() {
+        openUserModal();
+    });
 
-        document.addEventListener('DOMContentLoaded', () => {
-            const modal = document.getElementById('user-modal');
-            const addUserBtn = document.querySelector('.add-user-btn');
-            const closeBtn = document.querySelector('.close-btn');
-            const userForm = document.getElementById('user-form');
+    // Función para abrir el modal de usuario
+    function openUserModal(user = {}) {
+        document.getElementById('modal-title').innerText = user.id ? 'Editar Usuario' : 'Agregar Usuario';
+        document.getElementById('modal-action').value = user.id ? 'update' : 'create';
+        document.getElementById('user-id').value = user.id || '';
+        document.getElementById('user-name').value = user.nombre || '';
+        document.getElementById('user-email').value = user.correo || '';
+        document.getElementById('user-modal').style.display = 'block';
+    }
 
-            addUserBtn.onclick = () => {
-                openUserModal();
-            }
+    // Agregar eventos al cargar la página
+    const modal = document.getElementById('user-modal');
+    const closeBtn = document.querySelector('.close-btn');
 
-            closeBtn.onclick = () => {
-                modal.style.display = 'none';
-            }
+    closeBtn.onclick = () => {
+        closeUserModal();
+    }
 
-            window.onclick = (event) => {
-                if (event.target == modal) {
-                  modal.style.display = 'none';
-                }
-            }
-        });
-
-        function openUserModal(user = {}) {
-            document.getElementById('modal-title').innerText = user.id ? 'Editar Usuario' : 'Agregar Usuario';
-            document.getElementById('modal-action').value = user.id ? 'update' : 'create';
-            document.getElementById('user-id').value = user.id || '';
-            document.getElementById('user-name').value = user.nombre || '';
-            document.getElementById('user-email').value = user.correo || '';
-            document.getElementById('user-role').value = user.rol || 'Administrador';
-            document.getElementById('user-modal').style.display = 'block';
+    window.onclick = (event) => {
+        if (event.target == modal) {
+            closeUserModal();
         }
+    }
+
+    function closeUserModal() {
+        document.getElementById('user-modal').style.display = 'none';
+    }
+});
 
